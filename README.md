a854882f7bd5c38d63ba211713e5e05ea4a2e19b
Jacob Scheid <jacobscheid@DEP-C02YL43ULVCF.lan1>
2019-08-28 17:49:20 -0700

    Added readme and cli options script

---
+++ README.md
@@ -0,0 +1,12 @@
+# AWS Security Scripts
+
+### A set of scripts to help with getting telemetry and security data out of our AWS infrastructure
+
+## Scripts
+
+### get-iam-report.sh
+
+A simple script that pulls all users in our AWS environment and provides:
+`user,arn,user_creation_time,password_enabled,password_last_used,password_last_changed,password_next_rotation,mfa_active,access_key_1_active,access_key_1_last_rotated,access_key_1_last_used_date,access_key_1_last_used_region,access_key_1_last_used_service,access_key_2_active,access_key_2_last_rotated,access_key_2_last_used_date,access_key_2_last_used_region,access_key_2_last_used_service,cert_1_active,cert_1_last_rotated,cert_2_active,cert_2_last_rotated`
+
+Helpful for seeing unused accounts and accounts without multifactor enabled.
--- get-iam-report.sh
+++ get-iam-report.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo '[!] ERROR: Must provide save path for report'
+    echo '[!] USAGE: ./get-iam-report.sh <path to save report>'
+    exit
+fi
+
 echo "[*] Let's check out the IAM report!"
 
 # Tell AWS to generate report
 echo '[-] Requesting report to be generated...'
 aws iam generate-credential-report
 
+echo '[-] Waiting for report to generate'
+sleep 2
+
 timestamp=$(date +%s)
 
 # Download the report
-echo '[-] Saving report to ~/Documents/reports/aws/aws_iam_report_<timestamp>.csv'
-aws iam get-credential-report --output text --query Content | base64 -D >> ~/Documents/Reports/aws/aws_iam_report_$timestamp.csv
+echo "[-] Saving report to $1/aws_iam_report_<timestamp>.csv"
+aws iam get-credential-report --output text --query Content | base64 -D >> $1/aws_iam_report_$timestamp.csv
 
 echo '[*] All done!'
